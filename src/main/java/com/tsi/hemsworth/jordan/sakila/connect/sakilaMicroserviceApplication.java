2ca0802e5ddd11473d78872133aa5dca90470add
Jordan Hemsworth <jordanhemsworth@gmail.com>
2022-07-27 19:34:25 +0100

    Add Film and Delete added

--- src/main/java/com/tsi/hemsworth/jordan/sakila/connect/sakilaMicroserviceApplication.java
+++ src/main/java/com/tsi/hemsworth/jordan/sakila/connect/sakilaMicroserviceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+
 @CrossOrigin("http://localhost:3306")
 @SpringBootApplication
 @RestController  //handles GET, POST, DELETE, PUT requests
@@ -12,14 +14,20 @@ import org.springframework.web.bind.annotation.*;
 public class sakilaMicroserviceApplication{
 
 	@Autowired
+	private FilmRepository filmRepository;
+
+	@Autowired
 	private ActorRepository actorRepository;
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(sakilaMicroserviceApplication.class, args);
 	}
 
-	public sakilaMicroserviceApplication(ActorRepository actorRepository){
+	public sakilaMicroserviceApplication(ActorRepository actorRepository, FilmRepository filmRepository){
 		this.actorRepository = actorRepository;
+		this.filmRepository = filmRepository;
 	}
 
 
@@ -44,4 +52,29 @@ public class sakilaMicroserviceApplication{
 	void deleteActorById(@RequestParam int id){
 		actorRepository.deleteById(id);
 	}
+
+	@GetMapping("/All_Films")
+	public @ResponseBody
+	Iterable<Film>getAllFilms(){
+		return filmRepository.findAll();
+	}
+
+	@PostMapping("/Add_New_Film")
+	public @ResponseBody String addNewFilm(@RequestParam String title, @RequestParam String description, @RequestParam Integer release_year, @RequestParam Integer language_id,
+										   @RequestParam Integer rental_duration,
+										   @RequestParam BigDecimal rental_rate, @RequestParam Integer length, @RequestParam BigDecimal replacement_cost,
+										   @RequestParam String rating, @RequestParam String special_features){
+		Film film = new Film(title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating, special_features);
+		filmRepository.save(film);
+		return "saved";
+	}
+
+	@DeleteMapping("/Delete_Film_By_Id")
+	public @ResponseBody
+	void deleteFilmById(@RequestParam Integer id){
+		filmRepository.deleteById(id);
+	}
+
+
+
 }
