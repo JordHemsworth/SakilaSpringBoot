a1893ebb886aaf9655e04df92c263051f6e6394b
Jordan Hemsworth <jordanhemsworth@gmail.com>
2022-08-09 12:48:42 +0100

    Added another test

--- src/test/java/com/tsi/hemsworth/jordan/sakila/connect/SimpleTest.java
+++ src/test/java/com/tsi/hemsworth/jordan/sakila/connect/SimpleTest.java
@@ -2,6 +2,7 @@
 package com.tsi.hemsworth.jordan.sakila.connect;
 
 import com.tsi.hemsworth.jordan.sakila.connect.actor.Actor;
+import com.tsi.hemsworth.jordan.sakila.connect.category.Category;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,5 +21,13 @@ public class SimpleTest {
         Actor testActor = new Actor();
         testActor.setLastName("Harden");
         assertEquals("Harden", testActor.getLastName(), "The name didn't match");
+    }
+
+    @Test
+    public void test_GetCategoryName(){
+        Category testCategory = new Category();
+        testCategory.setName("Actionee");
+        assertEquals("Actionee", testCategory.getName(), "The name didn't match");
+
     }
 }
