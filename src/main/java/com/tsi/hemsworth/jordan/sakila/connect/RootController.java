81310b064e95f3710b21455e04023e45c96a0cf7
JordHemsworth <jordanhemsworth+1@gmail.com>
2022-08-25 16:07:06 +0100

    Add Root Controller for HTTP Response

---
+++ src/main/java/com/tsi/hemsworth/jordan/sakila/connect/RootController.java
@@ -0,0 +1,14 @@
+package com.tsi.hemsworth.jordan.sakila.connect;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class RootController {
+
+    @GetMapping
+    public ResponseEntity<Void> getRoot(){
+        return ResponseEntity.status(200).build();
+    }
+
+}
