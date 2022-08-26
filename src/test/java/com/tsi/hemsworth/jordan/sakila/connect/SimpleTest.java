960ce21f5ecfe6a8e7c8d3500dbcc60a3651c7ac
JordHemsworth <jordanhemsworth+1@gmail.com>
2022-08-26 06:37:40 +0100

    Added JUNIT tests

--- src/test/java/com/tsi/hemsworth/jordan/sakila/connect/SimpleTest.java
+++ src/test/java/com/tsi/hemsworth/jordan/sakila/connect/SimpleTest.java
@@ -4,12 +4,17 @@ package com.tsi.hemsworth.jordan.sakila.connect;
 import com.tsi.hemsworth.jordan.sakila.connect.actor.Actor;
 import com.tsi.hemsworth.jordan.sakila.connect.category.Category;
 import com.tsi.hemsworth.jordan.sakila.connect.film.Film;
+import com.tsi.hemsworth.jordan.sakila.connect.filmActor.FilmActorKey;
+import com.tsi.hemsworth.jordan.sakila.connect.language.Language;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SimpleTest {
 
+    //
+    //Test Actor Setters
+    //
     @Test
     public void test_GetFirstName(){
         Actor testActor = new Actor();
@@ -25,6 +30,16 @@ public class SimpleTest {
     }
 
     @Test
+    public void test_GetActorId(){
+        Actor testActor = new Actor();
+        testActor.setId(1);
+        assertEquals(1, testActor.getId(), "The ID didn't match");
+    }
+
+    //
+    //Test Category Setters
+    //
+    @Test
     public void test_GetCategoryName(){
         Category testCategory = new Category();
         testCategory.setName("Actionee");
@@ -33,12 +48,64 @@ public class SimpleTest {
     }
 
     @Test
-    public void test_GetRating(){
-        Film testRating = new Film();
-        testRating.setRating("1");
-        assertEquals("1", testRating.getRating());
+    public void test_GetCategoryId(){
+        Category testCategory = new Category();
+        testCategory.setCategoryId(1);
+        assertEquals(1, testCategory.getCategoryId(), "The name didn't match");
     }
 
+    //
+    //Test Film Setters
+    //
+
+    @Test
+    public void test_GetFilmId(){
+        Film testRating = new Film();
+        testRating.setFilm_id(1);
+        assertEquals(1, testRating.getFilm_id());
+    }
+
+    @Test
+    public void test_GetFilmTitle(){
+        Film testRating = new Film();
+        testRating.setTitle("Harry P");
+        assertEquals("Harry P", testRating.getTitle());
+    }
+
+    @Test
+    public void test_GetFilmDesc(){
+        Film testRating = new Film();
+        testRating.setDescription("Harry P magic");
+        assertEquals("Harry P magic", testRating.getDescription());
+    }
+
+    @Test
+    public void test_GetFilmYear(){
+        Film testRating = new Film();
+        testRating.setRelease_year(2020);
+        assertEquals(2020, testRating.getRelease_year());
+    }
+
+    /*@Test
+    public void test_GetFilmLang(){
+        Film testRating = new Film();
+        testRating.setL("1");
+        assertEquals("1", testRating.getRating());
+    }*/
+
+    @Test
+    public void test_GetFilmRentDur(){
+        Film testRating = new Film();
+        testRating.setRental_duration(20);
+        assertEquals(20, testRating.getRental_duration());
+    }
+
+   /* @Test
+    public void test_GetFilmRentRate(){
+        Film testRating = new Film();
+        testRating.setRental_rate(1);
+        assertEquals(1, testRating.getRental_rate());
+    }*/
 
     @Test
     public void test_GetLength(){
@@ -46,4 +113,54 @@ public class SimpleTest {
         testLength.setLength(120);
         assertEquals(120, testLength.getLength());
     }
+
+    /*@Test
+    public void test_GetFilmCost(){
+        Film testLength = new Film();
+        testLength.setReplacement_cost(1);
+        assertEquals(1, testLength.getLength());
+    }*/
+
+    @Test
+    public void test_GetRating(){
+        Film testRating = new Film();
+        testRating.setRating("1");
+        assertEquals("1", testRating.getRating());
+    }
+
+    @Test
+    public void test_GetSpecFeatures(){
+        Film testRating = new Film();
+        testRating.setSpecial_features("1");
+        assertEquals("1", testRating.getSpecial_features());
+    }
+
+
+    //
+    // Test Language Setters
+    //
+
+    @Test
+    public void test_GetLangId(){
+        Language testRating = new Language();
+        testRating.setLanguageId(1);
+        assertEquals(1, testRating.getLanguageId());
+    }
+
+    @Test
+    public void test_GetLangName(){
+        Language testRating = new Language();
+        testRating.setName("English");
+        assertEquals("English", testRating.getName());
+    }
+
+    @Test
+    public void test_GetLangUpdate(){
+        Language testRating = new Language();
+        testRating.setLastUpdate("21/04/2020");
+        assertEquals("21/04/2020", testRating.getLastUpdate());
+    }
+
+
+
 }
